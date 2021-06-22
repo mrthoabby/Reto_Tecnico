7b53fd4d8de32f07f6ec7b5f1fc836fb19183128
Daniel Abbey <deanis89@hotmail.com>
2021-06-22 13:49:27 -0500

    Aplicando Principios SOLID a metodo LectoEjecutor de CC re-estructurado a Lector

--- ControladorFile.cs
+++ ControladorFile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -14,25 +15,24 @@ namespace ReadConcurrent
 	/// </remarks>
 	static class ControladorFile
 	{
-		private static ReaderWriterLockSlim blokeotoFile = new ReaderWriterLockSlim();
-
+		private static readonly ReaderWriterLockSlim blokeotoFile = new();
 
 		/// <summary>
-		/// Lee el archivo de recurso de texto y ejecuta las acciones establecidas
+		/// Lee el archivo de recurso de texto y retorna parcialmente partes del mismo usando yield
 		/// </summary>
-		internal static void LectoEjecutor(string proceso)
+		/// <returns></returns>
+		internal static IEnumerable<string> Lector()
 		{
+			string partialText = string.Empty;
 			using (FileStream flujodeTexto = File.Open(@"C:\Users\Danis\Documents\codigo\ReadConcurrent\el_quijote.txt" , FileMode.Open , FileAccess.Read , FileShare.Read))
 			{
 				byte[] conjuntodeBytes = new byte[1024];
-				UTF8Encoding  text = new(true);
 				while (flujodeTexto.Read(conjuntodeBytes , 0 , conjuntodeBytes.Length) > 0)
 				{
-					Console.WriteLine($"This is of" + text.GetString(conjuntodeBytes).Normalize());
+					yield return Encoding.UTF8.GetString(conjuntodeBytes).Normalize();
 				}
-
 			}
-
+			yield return partialText;
 		}
 
 		/// <summary>
@@ -42,15 +42,12 @@ namespace ReadConcurrent
 		internal async static void Escritor(string texto)
 		{
 			blokeotoFile.EnterWriteLock();
-			using (StreamWriter escritor = new StreamWriter(@"C:\Users\Danis\Documents\codigo\ReadConcurrent\registros.txt" , true))
+			using (StreamWriter escritor = new(@"C:\Users\Danis\Documents\codigo\ReadConcurrent\registros.txt" , true))
 			{
 				await escritor.WriteLineAsync(texto);
 			}
 			blokeotoFile.ExitWriteLock();
 		}
-
-
-
 
 	}
 }
