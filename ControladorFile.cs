8489be5eb2615328eccb7601e2aaab52befb40a4
Daniel Abbey <deanis89@hotmail.com>
2021-06-22 12:54:20 -0500

    Metodo Esctritor de CH creado y documentado

--- ControladorFile.cs
+++ ControladorFile.cs
@@ -1,40 +1,56 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ReadConcurrent
 {
-    /// <summary>
-    /// Clase controlador de archivos
-    /// Contiene todos los metos para acceder y leer o modificar los archivos de texto.
-    /// </summary>
-    /// <remarks>
-    /// Lee archivos de texto y los deja listo para su usuo
-    /// </remarks>
-    static class ControladorFile
-    {
+	/// <summary>
+	/// Clase controlador de archivos
+	/// Contiene todos los metos para acceder y leer o modificar los archivos de texto.
+	/// </summary>
+	/// <remarks>
+	/// Lee archivos de texto y los deja listo para su usuo
+	/// </remarks>
+	static class ControladorFile
+	{
+		private static ReaderWriterLockSlim blokeotoFile = new ReaderWriterLockSlim();
 
-        /// <summary>
-        /// Lee el archivo de recurso de texto y ejecuta las acciones establecidas
-        /// </summary>
-        internal static void LectoEjecutor()
+
+		/// <summary>
+		/// Lee el archivo de recurso de texto y ejecuta las acciones establecidas
+		/// </summary>
+		internal static void LectoEjecutor(string proceso)
 		{
-            string nameFile = @"C:\Users\Danis\Documents\codigo\ReadConcurrent\el_quijote.txt";
-            using(var flujodeTexto = File.Open(nameFile , FileMode.Open , FileAccess.Read , FileShare.Read))
+			using (FileStream flujodeTexto = File.Open(@"C:\Users\Danis\Documents\codigo\ReadConcurrent\el_quijote.txt" , FileMode.Open , FileAccess.Read , FileShare.Read))
 			{
-                byte[] conjuntodeBytes = new byte[1024];
-                UTF8Encoding  text = new(true);
-                while(flujodeTexto.Read(conjuntodeBytes,0,conjuntodeBytes.Length) > 0)
+				byte[] conjuntodeBytes = new byte[1024];
+				UTF8Encoding  text = new(true);
+				while (flujodeTexto.Read(conjuntodeBytes , 0 , conjuntodeBytes.Length) > 0)
 				{
-                    Console.WriteLine($"This is of"+text.GetString(conjuntodeBytes).Normalize());
+					Console.WriteLine($"This is of" + text.GetString(conjuntodeBytes).Normalize());
 				}
+
 			}
 
-        }
+		}
+
+		/// <summary>
+		/// Escribe información en el archivo de registros
+		/// </summary>
+		/// <param name="texto">Texto a escribir en el archivo de registros</param>
+		internal async static void Escritor(string texto)
+		{
+			blokeotoFile.EnterWriteLock();
+			using (StreamWriter escritor = new StreamWriter(@"C:\Users\Danis\Documents\codigo\ReadConcurrent\registros.txt" , true))
+			{
+				await escritor.WriteLineAsync(texto);
+			}
+			blokeotoFile.ExitWriteLock();
+		}
+
+
+
 
 	}
 }
