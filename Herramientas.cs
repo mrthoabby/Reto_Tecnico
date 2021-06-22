0f35989193e4e40a8abae6e464c977b7a27c6ddf
Daniel Abbey <deanis89@hotmail.com>
2021-06-22 16:54:18 -0500

    Metodos CleanText y GarbajeRecolectorText de CH creados y documentados

--- Herramientas.cs
+++ Herramientas.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace ReadConcurrent
 {
@@ -12,15 +8,37 @@ namespace ReadConcurrent
 	/// </summary>
 	static class Herramientas
 	{
+		internal static int words_end_In_N = 0;
+
+
+		private static readonly string[] character_garbage = new[]{"\r","\0","\t" };
+		private static readonly string[] spaces_garbage = new[] {"  ","   ","\n" };
+
 		/// <summary>
 		/// Cuenta las palabras que terminan en un determinado caracter
 		/// </summary>
 		/// <param name="texto">El texto en donce se contaran las palabras</param>
 		/// <param name="find">Caracter a encontrar al final de cada palabra</param>
 		/// <returns></returns>
-		internal static int contadorEnd(this string texto , char find)
+		internal static int ContadorEnd(this string texto , char find) => texto.ToLower().Split(" ").Count(x => x.Length > 0 && x[^1] == char.ToLower(find));
+		
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="texto"></param>
+		/// <returns></returns>
+		internal static string CleanText(this string texto, char exepcion = ' ')
 		{
-			return texto.ToLower().Split().Count(x => x[^1] ==char.ToLower(find));
+			return new(texto.GarbajeRecolectorText().Where(valor => valor == ' ' || valor == exepcion ||char.IsLetterOrDigit(valor)).ToArray());
+		}
+		private static string GarbajeRecolectorText(this string texto)
+		{
+			string textToClear = "";
+			for (int i = 0 ; i < 3 ; i++)
+			{
+				textToClear = texto.Replace(spaces_garbage[i] , " ");
+			}
+			return textToClear;
 		}
 	}
 }
